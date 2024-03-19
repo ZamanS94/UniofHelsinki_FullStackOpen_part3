b22609ed8af5908cf31abb49aa67941936db49e0
Sabina Zaman <sabina.zaman94@gmail.com>
2024-03-19 23:23:25 +0100

    render 8

--- part3/index.js
+++ part3/index.js
@@ -134,7 +134,7 @@ app.delete('/delete/:id', (request, response) => {
     response.send(`${id} Deleted!`)
 })
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 
