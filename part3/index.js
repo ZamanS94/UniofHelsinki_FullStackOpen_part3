dcef8cd92cc47efea69ffaf5982c42a567e772c4
Sabina Zaman <sabina.zaman94@gmail.com>
2024-03-26 10:09:57 +0100

    3.13, 3.14, Phonebook database, step 1, step 2

--- part3/index.js
+++ part3/index.js
@@ -1,162 +1,104 @@
+require('dotenv').config()
 const express = require('express')
-const { json } = require('express')
-const morgan = require('morgan')
+const path = require('path')
+const { phonebookSchema, Phonebook, mongoose } = require('./models/phonebook')
 
 const app = express()
-
-app.use(express.static('dist'))
-app.use(morgan('tiny'))
-
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    },
-    {
-        "id": 5,
-        "name": "Pinky",
-        "number": "39-23-6423122"
-    }
-]
+const PORT = process.env.PORT || 3001
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!!</h1>')
+app.use(express.static(path.join(__dirname, 'dist')))
+
+const counterSchema = new mongoose.Schema({
+  _id: String,
+  seq: { type: Number, default: 0 }
+})
+const Counter = mongoose.model('Counter', counterSchema)
+
+phonebookSchema.pre('save', async function(next) {
+  try {
+    if (!this.id) {
+      const counter = await Counter.findOneAndUpdate(
+        { _id: 'phonebookId' },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      )
+      this.id = counter.seq
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', async (request, response) => {
+  try {
+    const persons = await Phonebook.find({}).select('-_id -__v')
     response.json(persons)
+  } catch (error) {
+    response.status(500).json({ error: error.message })
+  }
 })
 
-
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
+app.post('/api/persons', async (request, response) => {
+  try {
+    const { name, number } = request.body
+      
+    if (!name || !number) {
+      throw ('Name and number are required')
     }
-    return i
-}
-
-function getCurrentDateTime() {
-    var now = new Date()
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    var day = days[now.getDay()]
-    var month = months[now.getMonth()]
-    var date = now.getDate()
-    var year = now.getFullYear()
-    var h = now.getHours()
-    var m = now.getMinutes()
-    var s = now.getSeconds()
-    m = checkTime(m)
-    s = checkTime(s)
-
-    return `${day} ${month} ${date} ${year} ${h}:${m}:${s}  GMT+0200 (Eastern European Standard Time)`
-}
-
-app.get('/info', (request, response) => {
-    const param1 = persons.length;
-    const param2 = getCurrentDateTime();
-    response.send(`Phonebook has info for ${param1} people <br/><br/>${param2}`)
+        
+    const person = new Phonebook({ name, number })
+    const savedPerson = await person.save()
+    response.json(savedPerson)
+  } catch (error) {
+    console.log(error)
+    return response.status(400).json({ error: error })
+  }
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: "Name or number is missing" })
+app.get('/api/persons/:id', async (request, response) => {
+  const { id } = request.params
+  try {
+    const person = await Phonebook.findOne({ id }).select('-_id -__v')
+    if (!person) {
+      return response.status(404).json({ error: 'Person not found' })
     }
-
-    let nameExists = persons.some(person => person.name === body.name)
-    if (nameExists) {
-        return response.status(400).json({ error: "Name must be unique" })
-    }
-
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    }
-    persons = persons.concat(person)
     response.json(person)
+  } catch (error) {
+    response.status(500).json({ error: error.message })
+  }
 })
 
-app.put('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const body = request.body
+app.put('/api/persons/:id', async (request, response) => {
+  const id = Number(request.params.id)
+  const body = request.body
 
-    const personToUpdate = persons.find(person => person.id === id)
-
-    if (!personToUpdate) {
-        return response.status(404).json({ error: 'Person not found' })
-    }
-
-    // Update only if name and number are provided in the request body
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'Name or number is missing' })
-    }
-
-    // Check if the new name is already in use by another person
-    const nameExists = persons.some(person => person.name === body.name && person.id !== id)
-    if (nameExists) {
-        return response.status(400).json({ error: 'Name must be unique' })
-    }
-
-    // Update the person object
+  try {
+    const personToUpdate = await Phonebook.findOne({ id })
     personToUpdate.name = body.name
     personToUpdate.number = body.number
-
-    response.json(personToUpdate)
+    const updatedPerson = await personToUpdate.save()
+    response.json(updatedPerson)
+  } catch (error) {
+    response.status(500).json({ error: error.message })
+  }
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).send('Person not found')
+app.delete('/api/persons/:id', async (request, response) => {
+  const id = Number(request.params.id)
+
+  try {
+    const deletedPerson = await Phonebook.findOneAndDelete({ id })
+    if (!deletedPerson) {
+      return response.status(404).json({ error: 'Person not found' })
     }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
     response.send(`${id} Deleted!`)
+  } catch (error) {
+    response.status(500).json({ error: error.message })
+  }
 })
 
-const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
-// We depolyed both backend and frontend to Render
-// Address: https://uniofhelsinki-fullstackopen-part3-2.onrender.com/
---
+++ part3/models/phonebook.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB')
+})
+.catch(error => {
+  console.error('Error connecting to MongoDB:', error.message)
+})
+
+const phonebookSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  name: String,
+  number: String,
+})
+
+const Phonebook = mongoose.model('Phonebook', phonebookSchema)
+
+module.exports = {
+  phonebookSchema: phonebookSchema,
+  Phonebook: Phonebook,
+  mongoose: mongoose
+}
